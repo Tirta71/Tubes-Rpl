bbe1116053b8dfba58abfaba8608e00457554dbc
tirta71 <tirta4132@gmail.com>
2023-05-30 09:19:27 +0700

    anjay

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../css/navbar.css";
 import { Link, useLocation } from "react-router-dom";
 import AvatarProfile from "../assets/michael-dam-mEZ3PoFGs_k-unsplash.jpg";
 import Background from "../assets/BackroundDasar.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronDown,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 import Hero from "./Hero";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 export default function Navbar() {
   const navStyle = {
@@ -17,14 +22,47 @@ export default function Navbar() {
 
   const location = useLocation();
 
+  const [isProfileOpen, setProfileOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const profileRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (profileRef.current && !profileRef.current.contains(event.target)) {
+        setProfileOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleProfileClick = () => {
+    setProfileOpen(!isProfileOpen);
+  };
+
+  const handleMenuClick = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
       <nav style={navStyle}>
         <div className="container container-navbar">
           <div className="logo">
             <span>LOGO</span>
+            <div className="menu-icon" onClick={handleMenuClick}>
+              {isMenuOpen ? (
+                <FontAwesomeIcon icon={faTimes} />
+              ) : (
+                <FontAwesomeIcon icon={faBars} />
+              )}
+            </div>
           </div>
-          <div className="my-navbar">
+          <div className={`my-navbar ${isMenuOpen ? "open" : ""}`}>
             <ul className="my-navbar-menu">
               <li>
                 <Link
@@ -57,16 +95,39 @@ export default function Navbar() {
             </ul>
           </div>
 
-          <div className="profile">
+          <div
+            className={`profile ${isMenuOpen ? "open" : ""}`}
+            ref={profileRef}
+            onClick={handleProfileClick}
+          >
             <div className="profile-img">
               <img src={AvatarProfile} alt="" />
             </div>
             <div className="profile-font">
               <span>Tirta Samara</span>
             </div>
+
             <div className="markdown-profile">
               <FontAwesomeIcon icon={faChevronDown} />
             </div>
+
+            <TransitionGroup>
+              {isProfileOpen && (
+                <CSSTransition classNames="profile-dropdown" timeout={300}>
+                  <div className="profile-dropdown">
+                    <ul>
+                      <li>
+                        <Link to="/">Profile Detail </Link>
+                      </li>
+
+                      <li>
+                        <Link to="/">Logout</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </CSSTransition>
+              )}
+            </TransitionGroup>
           </div>
         </div>
 
--- src/pages/PesanKamar.jsx
+++ src/pages/PesanKamar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import FormPesankamar from "../components/FormPesankamar";
 
 export default function PesanKamar() {
   return (
     <div>
       <Navbar />
+      <FormPesankamar />
       <Footer />
     </div>
   );
